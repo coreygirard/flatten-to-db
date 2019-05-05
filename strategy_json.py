ff79bccf95b28ae4c44be3d4907064ff17f58380
Corey Girard <corey.r.girard@gmail.com>
2019-05-05 12:50:07 -0400

    Remove slashes from generated objects

--- strategy_json.py
+++ strategy_json.py
@@ -6,12 +6,24 @@ from hypothesis.strategies import (
     none,
     recursive,
     text,
+    composite,
 )
+from hypothesis import assume
 from string import printable
+import json
 
 
-json_strategy = recursive(
-    none() | booleans() | floats() | text(printable),
+printable_without_slash = "".join(c for c in printable if c != "/")
+_json_strategy = recursive(
+    none() | booleans() | floats() | text(printable_without_slash),
     lambda children: lists(children, 1)
-    | dictionaries(text(printable), children, min_size=1),
+    | dictionaries(text(printable_without_slash), children, min_size=1),
 )
+
+
+@composite
+def json_strategy(draw):
+    example = draw(_json_strategy)
+    s = json.dumps(example)
+    # assume("/" not in s)
+    return s
