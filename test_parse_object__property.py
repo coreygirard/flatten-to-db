d51ff2655e3c81a82fc9c62a6d261f4e80f5bcce
Corey Girard <corey.r.girard@gmail.com>
2019-05-05 12:51:01 -0400

    Switch to object strategy

--- test_parse_object__property.py
+++ test_parse_object__property.py
@@ -1,18 +1,17 @@
 from pprint import pprint
 from collections import Counter
-from hypothesis import given
+from hypothesis import given, assume
+
+from parse_object import parse
+from strategy_object import object_strategy
 
 
-from parse_json import parse
-from strategy_json import json_strategy
-
-
-@given(json_strategy)
+@given(object_strategy())
 def test_doesnt_fail(j):
     parse(j)
 
 
-@given(json_strategy)
+@given(object_strategy())
 def test_is_list_of_maps(j):
     out = parse(j)
     assert isinstance(out, list)
@@ -23,7 +22,7 @@ def test_is_list_of_maps(j):
         # won't always have 'value', so don't check
 
 
-@given(json_strategy)
+@given(object_strategy())
 def test_parent_paths_have_counterparts(j):
     out = parse(j)
 
@@ -37,8 +36,52 @@ def test_parent_paths_have_counterparts(j):
         assert v >= paths[k]
 
 
-@given(json_strategy)
-def test_link_chain_to_root(j):
+@given(object_strategy())
+def test_path_uniqueness(j):
+    out = parse(j)
+    paths = [d["path"] for d in out]
+    assert len(paths) == len(set(paths))
+
+
+def add_to_tree(tree, path, value):
+    if path == []:
+        return value
+
+    crumb, *path = path
+    if crumb not in tree:
+        tree[crumb] = {}
+    tree[crumb] = add_to_tree(tree[crumb], path, value)
+    return tree
+
+
+def lists_to_dicts(data):
+    if isinstance(data, list):
+        return {str(i): lists_to_dicts(e) for i, e in enumerate(data)}
+    elif isinstance(data, dict):
+        return {k: lists_to_dicts(v) for k, v in enumerate(data)}
+    else:
+        return data
+
+
+@given(object_strategy())
+def test_can_rebuild(j):
     out = parse(j)
 
+    for e in out:
+        e["split_path"] = e["path"].split("/")[1:-1]
+
+    tree = {}
+    for e in out:
+        if "value" in e:
+            tree = add_to_tree(tree, e["split_path"], e["value"])
+
+    pprint(j)
+    print()
     pprint(out)
+    print()
+    pprint(tree)
+    print()
+
+    assert tree == lists_to_dicts(j)
+
+    print("\n" * 5)
