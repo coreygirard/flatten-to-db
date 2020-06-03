bc725cdf90133662e88c58bd4221a8c9892e650d
Corey Girard <corey.r.girard@gmail.com>
2020-06-04 10:28:21 +1200

    Eliminate strings coerceable to ints from potential dictionary keys

--- test/strategy_object.py
+++ test/strategy_object.py
@@ -12,11 +12,23 @@ from hypothesis import assume
 from string import printable
 import json
 
+
+def cant_become_int(s):
+    try:
+        int(s)
+        return False
+    except ValueError:
+        pass
+    return True
+
+
 printable_without_slash = "".join(c for c in printable if c != "/")
 _object_strategy = recursive(
     none() | booleans() | floats() | text(printable_without_slash),
     lambda children: lists(children, 1)
-    | dictionaries(text(printable_without_slash), children, min_size=1),
+    | dictionaries(
+        text(printable_without_slash).filter(cant_become_int), children, min_size=1
+    ),
 )
 
 
